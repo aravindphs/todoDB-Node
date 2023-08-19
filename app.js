cdde13dec84f735905b67c62aa903a47d5d61fc5
Aravind Kumar J <aravind94977034@gmail.com>
2023-08-19 18:55:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -131,6 +131,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || port, () => {
+  console.log(`Your server started on port ${port}`);
+})
